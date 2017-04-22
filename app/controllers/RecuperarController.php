a4eea9897f51f94170ffaf25a78d0fe0b0758aef
Marcus_Vinicius <mvso2355@gmail.com>
2017-04-22 15:56:57 -0300

    Solicitação de link para redefinir senha

--- app/controllers/RecuperarController.php
+++ app/controllers/RecuperarController.php
@@ -31,22 +31,56 @@ class RecuperarController extends \HXPHP\System\Controller
 		if (!is_null($email) && $email !== false) {
 			$validar = Recovery::validar($email);
 
-			if ($validar->status===false) {
-				$error= $this->messages->getByCode($validar->code);
+				if ($validar->status === false) {
+					$error= $this->messages->getByCode($validar->code);
+				}
+				else{
+					$this->load(
+						'Services\PasswordRecovery',
+						$this->configs->site->url . $this->configs->baseURI .'recuperar/redefinir/'
+					);
+
+					Recovery::create(array(
+						'user_id'=>$validar->user->id,
+						'token'=>$this->passwordrecovery->token,
+						'status' => 0
+					));
+					$message=$this->messages->messages->getBycode('link-enviado',array(
+						'message'=> array(
+							$validar->user->name,
+							$this->passwordrecovery->link,
+							$this->passwordrecovery->link
+						)
+					));
+					$this->load('Services\Email');
+
+					$envioDoEmail=$this->email->send(
+						$validar->user->email,
+						'HXPHP -' . $message ['subject'],
+						$message['message'] . 'HXPHP',
+						array(
+							'email'=>$this->configs->mail->from_mail,
+							'remetente'=>$this->configs->mail->from
+						)
+					);
+				if($envioDoEmail===false){
+					$error= $this->messages->getByCode('email-nao-enviado');
+				}
 			}
 		}
 		else{
 			$error= $this->messages->getByCode('nenhum-usuario-encontrado');
-		}
+			}
+
 		if(!is_null($error)){
 			$this->load('Helpers\Alert', $error);
 		}
-		else{
-			$success =$this->messages->getByCode('link-enviado');
-			$this->view->setFile('blank');
+			else{
+				$success =$this->messages->getByCode('link-enviado');
+				$this->view->setFile('blank');
 
-			$this->load('Helpers\Alert', $success);
-		}
+				$this->load('Helpers\Alert', $success);
+			}
 	}
 
 	public function redefinirAction($token)

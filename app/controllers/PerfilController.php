9afd294cd90cc345d0bb3bfc756a92dbf35aef49
Marcus_Vinicius <mvso2355@gmail.com>
2017-04-25 18:51:52 -0300

    Add fields

---
+++ app/controllers/PerfilController.php
@@ -0,0 +1,35 @@
+<?php 
+
+class PerfilController extends \HXPHP\System\Controller
+{
+	public function __construct($configs)
+	{
+		parent::__construct($configs);
+
+		$this->load(
+			'Services\Auth',
+			$configs->auth->after_login,
+			$configs->auth->after_logout,
+			true
+		);
+
+		$this->auth->redirectCheck();
+		$this->load(
+			'Helpers\Menu',
+			$this->request,
+			$this->configs,
+			$this->auth->getUserRole()
+
+			);
+
+		$user_id = $this->auth->getUserId();
+
+		$this->view->setTitle('HXPHP - Editar Perfil')
+					->setVar('user', User::find($user_id));
+	}
+
+	public function editarAction()
+	{
+		
+	}
+}

7988c216a2de74d543dbd1c65b3589dbafe7a17b
Marcus_Vinicius <mvso2355@gmail.com>
2017-04-25 19:56:10 -0300

    Add action atualizar

--- app/controllers/PerfilController.php
+++ app/controllers/PerfilController.php
@@ -32,4 +32,34 @@ class PerfilController extends \HXPHP\System\Controller
 	{
 		
 	}
+
+	public function atualizarAction()
+	{
+		$user_id = $this->auth->getUserId();
+		
+
+		$this->request->setCustomFilters(array(
+			'email' => FILTER_VALIDATE_EMAIL
+		));
+
+		$post = $this->request->post();
+
+		if (!empty($post)) {
+			$atualizarUsuario = User::atualizar($user_id,$post);
+
+			if ($atualizarUsuario->status === false) {
+				$this->load('Helpers\Alert', array(
+					'danger',
+					'Ops! Não foi possível atualizar seu perfil. <br> Verifique os erros abaixo:',
+					$atualizarUsuario->errors
+				));
+			}
+			else {
+				$this->load('Helpers\Alert', array(
+					'success',
+					'Uhuul! Perfil atualizado com sucesso.'
+				));
+			}
+		}
+	}
 }
